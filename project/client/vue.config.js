6e3dd1c525964b9818b342ccebe9c2ed29400f01
sxf51 <sxf5166@163.com>
2025-05-07 20:12:27 +1000

    mod client

--- project/client/vue.config.js
+++ project/client/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true, 
-  client: {
-    webSocketURL: 'auto://0.0.0.0:0/ws' // 自动协议，正式开发时不要这么做
-  }
+  //client: {
+  //  webSocketURL: 'auto://0.0.0.0:0/ws' // 自动协议，正式开发时不要这么做
+  //}
 })
